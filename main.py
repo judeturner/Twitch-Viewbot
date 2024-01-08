17701fbdae53ba44cd32041247896b38a0ef2880
Jude <94721476+judeturner@users.noreply.github.com>
2024-01-07 16:53:16 -0800

    Changed tag in main.py

--- main.py
+++ main.py
@@ -14,19 +14,6 @@ import time
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-def check_for_updates():
-    try:
-        r = requests.get("https://raw.githubusercontent.com/judeturner/Windows-Twitch-Viewbot/main/version.txt")
-        remote_version = r.content.decode('utf-8').strip()
-        local_version = open('version.txt', 'r').read().strip()
-        if remote_version != local_version:
-            print("A new version is available. Please download the latest version from GitHub.")
-            time.sleep(3)
-            return False
-        return True
-    except:
-        return True
-
 def save_settings(twitch_username, set_160p):
     with open('settings.txt', 'w') as file:
         file.write(f"Twitch Username: {twitch_username}\n")
@@ -76,9 +63,6 @@ def set_stream_quality(driver, quality):
 
 
 def main():
-    if not check_for_updates():
-        return
-
     twitch_username, set_160p = load_settings()
 
 def print_announcement():
@@ -93,8 +77,6 @@ def print_announcement():
 
 
 def main():
-    if not check_for_updates():
-        return
     print_announcement()
     
     twitch_username, set_160p = load_settings()
@@ -103,15 +85,15 @@ def main():
 
     print(Colorate.Vertical(Colors.green_to_cyan, Center.XCenter("""
            
-                       ▄████████ ▄███████    █▄       ▄   ▄██▀▀▀▀   █     █▄
-                       ███    █▀ ███   ██▄    ██      ██   ██      ██     ██
-                       ███    █  ███▌  ███    █▀      █▄   ██       █     █
-                       █████▀▀▀  ███▌  ▄██    ██   ▄  █    ██ ▄▄     █   █
-                       ▄██       ▄██▌  ██▄    █   █▀  █    ███▀       █ ██
-                       ███       ███   ███    █▄ █ ██ █    ██         ██ 
-                       ▄██       ███   ▄██    ███   ███▄   ██         ▄█ 
-                       ███       ▄████████    █▀    ███    █▀         ██▄
-                       ▀                                              █▀
+    ██▄▄██████▄        █▄     █▄████▄██    ████       █   ▄█▄██▀███▄  █▄███████
+        ███      ███    ██▄    ██     █▄   ▄█  ██    ██   █▄           ██     █▄
+        ██▄      ██     ███    █▀     ▄█   ██   █    ██   ▀█           █▀     ▄█
+        ███      ▄██    ██     ██▄█▄██▄    ██   ██   ▀█   ██▄█▀███▄█   ██▄█▄██▄
+        ▄██      ▄██▌   ██▄    █     █     ██    █    █   ▄█       ▀   █     █
+        ███       ███   ███    █▄     █    ██    ██   █▀  ██           █▄     █
+        ▄██       ███   ▄██    ███    ▀█   ▄█     █   █   ██           ███    ▀█
+        ██▄       ▄████████    █▀      █▄  ██     ███▀    ▀████▄██▄█   █▀      █▄
+        ▀
                        """)))
     announcement = print_announcement()
     print("")
@@ -150,15 +132,15 @@ def main():
     os.system("cls")
     print(Colorate.Vertical(Colors.green_to_cyan, Center.XCenter("""
            
-                       ▄████████ ▄███████    █▄       ▄   ▄██▀▀▀▀   █     █▄
-                       ███    █▀ ███   ██▄    ██      ██   ██      ██     ██
-                       ███    █  ███▌  ███    █▀      █▄   ██       █     █
-                       █████▀▀▀  ███▌  ▄██    ██   ▄  █    ██ ▄▄     █   █
-                       ▄██       ▄██▌  ██▄    █   █▀  █    ███▀       █ ██
-                       ███       ███   ███    █▄ █ ██ █    ██         ██ 
-                       ▄██       ███   ▄██    ███   ███▄   ██         ▄█ 
-                       ███       ▄████████    █▀    ███    █▀         ██▄
-                       ▀                                              █▀   
+    ██▄▄██████▄        █▄     █▄████▄██    ████       █   ▄█▄██▀███▄  █▄███████
+        ███      ███    ██▄    ██     █▄   ▄█  ██    ██   █▄           ██     █▄
+        ██▄      ██     ███    █▀     ▄█   ██   █    ██   ▀█           █▀     ▄█
+        ███      ▄██    ██     ██▄█▄██▄    ██   ██   ▀█   ██▄█▀███▄█   ██▄█▄██▄
+        ▄██      ▄██▌   ██▄    █     █     ██    █    █   ▄█       ▀   █     █
+        ███       ███   ███    █▄     █    ██    ██   █▀  ██           █▄     █
+        ▄██       ███   ▄██    ███    ▀█   ▄█     █   █   ██           ███    ▀█
+        ██▄       ▄████████    █▀      █▄  ██     ███▀    ▀████▄██▄█   █▀      █▄
+        ▀   
                                                                     """)))
     print('')
     print('')
@@ -202,22 +184,20 @@ if __name__ == '__main__':
     main()
 
 # ==========================================
-# Copyright 2023 Powfy
+# Copyright 2023 Jude Turner
 
-# Permission must be granted through Powfy to use
-# this software and it's associated documentation files.
-
-# Powfy retains the entire rights for this software and at any point is able
-# to revoke access to the software. By purchasing, you are allowed to use
-# the software for its intended purpose but in no circumstances are allowed
-# to copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
